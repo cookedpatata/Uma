ad9f039e08d2ab4cb0070fcf7b5c11f25df8aebd
cookedpatata <jacenlo7@gmail.com>
2026-01-20 19:47:05 +0100

    commit7

--- php/PDO.php
+++ php/PDO.php
@@ -6,23 +6,23 @@ $baseDatos = 'uma';
 
 $conexion = new PDO('mysql:host=' . $servidor . '; dbname=' . $baseDatos, $usuario, $contraseña);
 
-function consulta($conexion,$sql){
-    try{
-        $consulta= $conexion->prepare($sql);
-        $consulta->execute();
+function consulta($pdo, $sql, $colum)
+{
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $c = 0;
+    while ($fila = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $datos[$colum[$c]] = $fila;
     }
-    catch(PDOException $e){
-        echo "Error: " . $e->getMessage();  
-    }
+    return $datos;
 }
 
-function insertar($conexion,$sql){
-    try{
-        $consulta= $conexion->prepare($sql);
+function insertar($conexion, $sql)
+{
+    try {
+        $consulta = $conexion->prepare($sql);
         $consulta->execute();
-    }
-    catch(PDOException $e){
-        echo "Error: " . $e->getMessage();  
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
 }
-?>
